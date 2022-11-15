0af4fee166a09f7e87df2e3e3420ec0edf0f7431
zheng <2642198391@qq.com>
2022-11-15 20:20:25 +0800

    2022/11/15

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,34 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
-  // configureWebpack: {
-  //   externals: {
-  //     // 基本格式：
-  //     // '包名' : '在项目中引入的名字'
-  //     echarts: 'echarts',
-  //     vue: 'Vue',
-  //     'vue-router': 'VueRouter',
-  //     vuex: 'Vuex',
-  //     axios: 'axios',
-  //     'element-ui': 'ELEMENT',
-  //     'vuex-persistedstate': 'createPersistedState',
-  //     'core-js': 'core-js',
-  //     'vue-aplayer': 'APlayer'
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  configureWebpack: {
+    externals: {
+      //     // 基本格式：
+      //     // '包名' : '在项目中引入的名字'
+      //     echarts: 'echarts',
+      vue: 'Vue',
+      'vue-router': 'VueRouter',
+      vuex: 'Vuex'
+      //     axios: 'axios',
+      //     'element-ui': 'ELEMENT',
+      //     'vuex-persistedstate': 'createPersistedState',
+      //     'core-js': 'core-js',
+      // 'vue-aplayer': 'APlayer'
 
-  //   }
-  // }
-  // target: 'web'
-  // devServer: {
-  //   proxy: {
-  //     '/zhx': {
-  //       target: 'https://b2de4f056142.akimitsu.top/', // 你需要跨域的url
-  //       changeOrigin: true,
-  //       ws: true,
-  //       pathRewrite: {
-  //         '^/zhx': ''
-  //       }
-  //     }
-  //   }
-  // }
+      //   }
+      // }
+      // target: 'web'
+      // devServer: {
+      //   proxy: {
+      //     '/zhx': {
+      //       target: 'https://b2de4f056142.akimitsu.top/', // 你需要跨域的url
+      //       changeOrigin: true,
+      //       ws: true,
+      //       pathRewrite: {
+      //         '^/zhx': ''
+      //       }
+      //     }
+    }
+  }
 })
